70a32858babe86c0334c949f4e7e3bbeca489be2
Lincianyong <110042648+Lincianyong@users.noreply.github.com>
2023-05-04 21:39:25 +0700

    link firebase

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import React from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
 
+import { useState } from "react";
+import './App.css';
+import { db } from './firebase-config';
+import { collection, getDocs } from "firebase/firestore";
+
 function App() {
+  const [events, setEvents] = useState([]);
+  const eventsCollectionRef = collection(db, "event");
+
+  const getEvents = async () => {
+    const data = await getDocs(eventsCollectionRef);
+    console.log(data.docs);
+    setEvents(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+  getEvents(); // fetch data when the component is rendered
+
+  console.log(events);
+
   return (
     <div className="">
       <header>
         <div className='-z-10 fixed mt-[-80vh]'>
-          <Header/>
+          <Header />
         </div>
         <div className='mt-[80vh]'>
-         <Body/>
+          <Body />
         </div>
       </header>
     </div>
@@ -19,3 +37,17 @@ function App() {
 }
 
 export default App;
+
+    // <div className="App">
+    //   {events.map((event) => {
+    //   return( 
+    //     <div>
+    //       {" "}
+    //       <h1>Name: {event.name}</h1>
+    //       <h1>Date: {event.date}</h1>
+    //       <h1>Location: {event.location}</h1>
+        
+    //    </div>
+    //   );
+    // })}
+    // </div>
